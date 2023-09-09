aba5c94c518ff0fcf4838dd4311f2d5c5dc6d365
AnnyTheExplorer <agbataanastasia@gmail.com>
2023-09-09 16:39:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const currentDate = new Date();
+const currentDay = daysOfWeek[currentDate.getUTCDay()];
+const currentUTCTime = currentDate.toUTCString();
+document.querySelector(
+  '[data-testid="currentDayOfTheWeek"]'
+).textContent = `Current Day Of The Week: ${currentDay}`;
+document.querySelector(
+  '[data-testid="currentUTCTime"]'
+).textContent = `Current UTC Time: ${currentUTCTime}`;
